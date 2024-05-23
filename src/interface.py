bfd17967b3986636670048c65fdf7ef6ce1af4f0
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2024-05-23 00:19:58 -0500

    Adding basic logic to convert_file method

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from pathlib import Path
 
 class MyGUI:
 
@@ -24,11 +25,11 @@ class MyGUI:
 
         self.file_to_convert = tk.StringVar()
 
-        self.ratio_button_jpeg = tk.Radiobutton(self.frame, text="JPEG", value="JPEG", variable=self.file_to_convert)
-        self.ratio_button_png = tk.Radiobutton(self.frame, text="PNG", value="PNG", variable=self.file_to_convert)
-        self.ratio_button_pdf = tk.Radiobutton(self.frame, text="PDF", value="PDF", variable=self.file_to_convert)
-        self.ratio_button_svg = tk.Radiobutton(self.frame, text="SVG", value="SVG", variable=self.file_to_convert)
-        self.ratio_button_mp4 = tk.Radiobutton(self.frame, text="MP4", value="MP4", variable=self.file_to_convert)
+        self.ratio_button_jpeg = tk.Radiobutton(self.frame, text="JPEG", value="jpeg", variable=self.file_to_convert)
+        self.ratio_button_png = tk.Radiobutton(self.frame, text="PNG", value="png", variable=self.file_to_convert)
+        self.ratio_button_pdf = tk.Radiobutton(self.frame, text="PDF", value="pdf", variable=self.file_to_convert)
+        self.ratio_button_svg = tk.Radiobutton(self.frame, text="SVG", value="svg", variable=self.file_to_convert)
+        self.ratio_button_mp4 = tk.Radiobutton(self.frame, text="MP4", value="mp4", variable=self.file_to_convert)
 
         self.ratio_button_jpeg.grid(row=0, column=0, sticky="news")
         self.ratio_button_png.grid(row=0, column=1, sticky="news")
@@ -55,4 +56,6 @@ class MyGUI:
         print(f"Selected: ${file_name}")
 
     def convert_file(self, file_path, file_type):
-        pass
+        path = Path(file_path)
+        new_file = path.with_suffix('.' + file_type)
+        return new_file
